67ee77961aeb3966adb891293ed35c7612be4cda
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-08-24 05:29:31 +0700

    Update ClearCounter.cs

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +24,9 @@ public class ClearCounter : MonoBehaviour
     public void Interact()
     {
         Debug.Log("Intertact");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }
