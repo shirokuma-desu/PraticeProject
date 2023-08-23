6b8e2a3f6c9508f3bf22855b8fbe2f8ceb9df7bf
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-08-24 05:29:59 +0700

    Create new KitchenObject
    Create new

---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public KitchenObjectSO GetKitchenObjectSO() { return kitchenObjectSO; } 
+}
