249dd7da8a783cca72e3043f1c32a46a0791751e
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-08-29 21:17:06 +0700

    Add Delivery Counter

---
+++ Assets/Scripts/Counter/DeliveryCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchentObject))
+            {
+                //Only accept plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
