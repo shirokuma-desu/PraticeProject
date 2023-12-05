a7113587feb5b6644c3fdfa6ce9730bf143675b4
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-12-05 17:31:48 +0700

    create

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        MainMenuScene,
+        GameScene,
+        LoadingScene,
+    }
+
+    private static Scene targetScene;
+    public static void Load(Scene targetSceneName)
+    {
+        Loader.targetScene = targetSceneName;
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+
+      
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+} 
