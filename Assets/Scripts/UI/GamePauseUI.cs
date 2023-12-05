e34ea67d93e0077646f273f400bcb19dbaddd6da
Đạt <77010056+shirokuma-desu@users.noreply.github.com>
2023-12-05 17:32:23 +0700

    create ui
    main menu, game pause, options

---
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePauseUI : MonoBehaviour
+{
+
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
+
+    private void Awake()
+    {
+        resumeButton.onClick.AddListener(() =>
+        {
+            GameManager.Instance.togglePauseGame();
+        });
+
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
+        optionsButton.onClick.AddListener(() =>
+        {
+            OptionUI.Instance.Show();
+        });
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnGamePaused += GameManager_OngamePaused;
+        GameManager.Instance.OnGameUnPaused += GameManager_OngameUnPaused;
+
+        Hide();
+    }
+
+    private void GameManager_OngameUnPaused(object sender, EventArgs e)
+    {
+        Hide();
+    }
+
+    private void GameManager_OngamePaused(object sender, EventArgs e)
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/OptionUI.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionUI : MonoBehaviour
+{
+    public static OptionUI Instance { get; private set; }
+
+    [SerializeField] private Button soundEffectButton;
+    [SerializeField] private Button musicButton;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private TextMeshProUGUI soundEffectsText;
+    [SerializeField] private TextMeshProUGUI musicText;
+
+    private void Awake()
+    {
+        soundEffectButton.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.ChangeVolume();
+            UpdateVisual();
+        });
+        musicButton.onClick.AddListener(() =>
+        {
+            MusicManager.instance.ChangeVolume();
+            UpdateVisual();
+        });
+
+        closeButton.onClick.AddListener(() =>
+        {
+            Hide();
+        });
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnGameUnPaused += KitchenGameManager_OnGameUnpaused;
+        UpdateVisual();
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnGameUnpaused(object sender, EventArgs e)
+    {
+        Hide();
+    }
+
+    private void UpdateVisual()
+    {
+        soundEffectsText.text = "Sound Efects: "+ Mathf.Round(SoundManager.Instance.GetVolume()*10f);
+        musicText.text = "Music: " + Mathf.Round(MusicManager.instance.GetVolume() * 10f);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
